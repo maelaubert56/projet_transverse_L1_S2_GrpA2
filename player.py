f8a02b8794ff84466bd0e5aa06d54ce668f6d632
SylvaBHD2 <93947829+SylvaBHD2@users.noreply.github.com>
2022-01-31 21:49:34 +0100

    shuriken impact sur player, + début des équipes + collisions entre sprite quasi parfait + pbs lors de la mort du joueur résolu

--- player.py
+++ player.py
@@ -2,39 +2,57 @@ import time
 
 import pygame
 import DEFAULT
-import random
+from random import randint
 from weapon import Weapon
-from math import cos,sin
+from math import cos, sin
 
 
 class Player(pygame.sprite.Sprite):
-    def __init__(self, game):
+    def __init__(self, game, team: int):
         super().__init__()
         self.game = game
-        # attrributs de jeu
+        # attributs de jeu
         self.health = 100
         self.max_health = 100
         self.velocity = DEFAULT.players_velocity
         self.accel = 0.2
         self.fall_velocity = 3
-        # image et cordonées
-        self.image = pygame.image.load(DEFAULT.path_player)
+        # equipes bleu pour 0 et 1 pour rouge
+        self.team = team
+        self.equipe_adverse = None
+        # image et coordonées
+        self.image = pygame.image.load(DEFAULT.img_team[self.team])
         self.image = pygame.transform.scale(self.image, (30, 30))
         self.rect = self.image.get_rect()
         # projectiles
         self.all_projectiles = pygame.sprite.Group()
-        self.direction = 0 # 0: gauche, 1: droite
-        # debuggage et masks
-        self.rect.x = 50 + random.randint(0, DEFAULT.window_width - 100)
-        self.rect.y = -100
+        self.direction = 0  # 0: gauche, 1: droite
+        self.bool_equiped = False
+        # debogage et masks
+        self.rect.x = randint(10, DEFAULT.window_width - 100)
+        self.rect.y = 50
         self.mask = pygame.mask.from_surface(self.image)
 
     def collision(self, screen):
         collision_terrain = pygame.sprite.collide_mask(self.game.object_background, self)
-        # collision_joueur = pygame.sprite.collide_mask(self.game.object_player, self)
+        collision_joueur = None
+        for elt in self.equipe_adverse:
+            print(" elt est ", elt)
+        if self.equipe_adverse is not None:
+            print(" ennemis et tt :", len(self.equipe_adverse), "bloods", len(self.game.all_players_red), " crips ", len(self.game.all_players_blue))
+            collision_joueur = pygame.sprite.spritecollide(self, self.equipe_adverse, False, pygame.sprite.collide_mask)
         if collision_terrain is not None:
-            if DEFAULT.DEBUG: pygame.draw.circle(surface=screen, color=(255, 0, 0), center=(collision_terrain[0] + 50, collision_terrain[1]), radius=5)
-            return collision_terrain
+            if DEFAULT.DEBUG:
+                pygame.draw.circle(surface=screen, color=(255, 0, 0),
+                                   center=(collision_terrain[0] + 50, collision_terrain[1]), radius=5)
+                return collision_terrain
+
+        elif collision_joueur is not None and len(collision_joueur) != 0 :
+            if DEFAULT.DEBUG:
+                pygame.draw.circle(surface=screen, color=(0, 255, 0),
+                                   center=(collision_joueur[0] + 50, collision_joueur[1]), radius=5)
+                print("collision :", collision_joueur)
+                return collision_joueur
         return False
 
     def fall(self, screen):
@@ -48,16 +66,20 @@ class Player(pygame.sprite.Sprite):
                 # reset velocity de la chute et la vitesse de chute
                 self.fall_velocity = 3
                 # témoin de collision
-                if DEFAULT.DEBUG : pygame.draw.circle(surface=screen, color=(255, 0, 0), center=(collision[0] + 50, collision[1]), radius=5)
+                if DEFAULT.DEBUG:
+                    pygame.draw.circle(surface=screen, color=(255, 0, 0),
+                                       center=(collision[0] + 50, collision[1]), radius=5)
         else:
+            if self.game.player_choice == self:
+                # changement de personnage
+                self.game.change_player_choice()
             # tuer le perso s'il touche l'eau
             self.kill()
 
     def show_life(self, surface):
         police = pygame.font.SysFont("monospace", 15)
-        image_texte = police.render(str(self.health), 1, (250, 0, 0))
-        surface.blit(image_texte, (self.rect.x + 10, self.rect.y - 20))
-
+        image_texte = police.render(str(self.health), True, (250, 0, 0))
+        surface.blit(image_texte, (self.rect.x + 5, self.rect.y - 20))
 
     def move_right(self, screen):
         self.direction = 1
@@ -69,7 +91,7 @@ class Player(pygame.sprite.Sprite):
                 # translation de la différence entre le bas et le point de collision (vecteur de déplacement)
                 self.rect.y = collision[1] - self.rect.height
             # débloque le perso bloqué
-            elif collision[0] < self.rect.x-self.rect.width:
+            elif collision[0] < self.rect.x - self.rect.width:
                 self.rect.x += self.velocity
 
     def move_left(self, screen):
@@ -82,34 +104,38 @@ class Player(pygame.sprite.Sprite):
                 # translation de la différence entre le bas et le point de collision (vecteur de déplacement)
                 self.rect.y = collision[1] - self.rect.height
             # débloque le perso bloqué
-            elif collision[0] > self.rect.x-self.rect.width:
+            elif collision[0] > self.rect.x - self.rect.width:
                 self.rect.x -= self.velocity
 
-
-    def jump(self,screen):
-        v0, g, t, jumping = 6.3*10, 19, 0, False
+    def jump(self, screen):
+        v0, g, t, jumping = 6.3 * 10, 19, 0, False
         x0, y0 = self.rect.x, self.rect.y
-        if self.direction == 1 : a = 1.31
-        else : a = 1.83
+        if self.direction == 1:
+            a = 1.31
+        else:
+            a = 1.83
 
         while t < 1:
-            t += 1/1000
+            t += 1 / 1000
             collision = self.collision(screen)
-            if collision==False or jumping == False or t<= 0.2:
+            if collision == False or jumping == False or t <= 0.2:
                 self.rect.x = x0 + v0 * cos(a) * t
-                self.rect.y = y0 -(-0.5 * g * t*t + v0 * sin(a) * t)
+                self.rect.y = y0 - (-0.5 * g * t * t + v0 * sin(a) * t)
                 jumping = True
-                #print("t=",round(t,2),"x=",self.rect.x,"y=",self.rect.y)
+                # print("t=",round(t,2),"x=",self.rect.x,"y=",self.rect.y)
 
             else:
                 jumping = False
-                t=1
+                t = 1
 
-
-
+    def equip_weapon(self, var=False):
+        """la variable sert a ranger ou sortir l'arme"""
+        if var:
+            self.bool_equiped = True
+            return 1
+        self.bool_equiped = False
+        return 0
 
     def launch_projectile(self):
         # nouveau projectile
         self.all_projectiles.add(Weapon(self))
-
-
--- weapon.py
+++ weapon.py
@@ -2,6 +2,7 @@ import pygame
 import DEFAULT
 from background_file import Background
 
+
 class Weapon(pygame.sprite.Sprite):
 
     def __init__(self, player):
@@ -9,12 +10,17 @@ class Weapon(pygame.sprite.Sprite):
         self.dmg = 100
         self.velocity = 10
         # image
+        self.player = player
         self.direction = player.direction
         self.image = pygame.image.load('assets/shuriken.png')
         self.image = pygame.transform.scale(self.image, (15, 15))
         # position
         self.rect = self.image.get_rect()
-        self.rect.x, self.rect.y = player.rect.x+20, player.rect.y
+        if self.player.direction == 0:
+            self.rect.x, self.rect.y = player.rect.x - self.player.rect.width, player.rect.y
+        else:
+            self.rect.x, self.rect.y = player.rect.x + self.player.rect.width, player.rect.y
+        # image
         self.origin_img = self.image
         self.angle = 0
 
@@ -23,14 +29,20 @@ class Weapon(pygame.sprite.Sprite):
 
     def rotate(self):
         self.angle += 7
-        self.image = pygame.transform.rotozoom(self.origin_img,self.angle,1)
+        self.image = pygame.transform.rotozoom(self.origin_img, self.angle, 1)
         self.rect = self.image.get_rect(center=self.rect.center)
 
     def move(self):
         collision = pygame.sprite.collide_mask(self.object_background, self)
+        collision_joueur = pygame.sprite.spritecollide(self, self.player.game.all_players, False,
+                                                       pygame.sprite.collide_mask)
+        print("collsion_joueur",collision_joueur)
         if collision is not None:
             self.kill()
             print("collision projectil terrain")
+        elif len(collision_joueur) != 0:
+            print("dégats sur l'ennemi")
+            self.kill()
         if self.rect.x > DEFAULT.window_width or self.rect.x < 0:
             self.kill()
             print("sortie de la fenetre")
