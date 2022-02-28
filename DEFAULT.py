076fd9ca32d4be9409ef6f1a783b9b4944083a00
mariuschev <marius.cheva@gmail.com>
2022-02-28 08:41:04 +0100

    *28/02/2022 0.0.31 agencement et positionement des boutons du jeu

--- DEFAULT.py
+++ DEFAULT.py
@@ -1,4 +1,4 @@
-DEBUG = True
+DEBUG = False
 
 # paramètres de la fenêtre
 window_name = "projet transverse"
--- menu.py
+++ menu.py
@@ -5,7 +5,7 @@ import DEFAULT
 
 class Menu():
     def __init__(self):
-
+        self.windows_height = Ground().rect.height+100
         self.previous_menu_number = 0
 
         # arriere plan
@@ -18,44 +18,44 @@ class Menu():
 
         # bouton settings
         self.settings_image = pygame.image.load(DEFAULT.path_settings)
-        self.settings_image = pygame.transform.scale(self.settings_image, (100, 100))
+        self.settings_image = pygame.transform.scale(self.settings_image, (120, 120))
         self.settings_rect = self.settings_image.get_rect()
-        self.settings_rect.x = DEFAULT.window_width / 2
-        self.settings_rect.y = 500
+        self.settings_rect.x = 1270
+        self.settings_rect.y = 0
 
         # bouton retour
         self.return_image = pygame.image.load(DEFAULT.path_return)
-        self.return_image = pygame.transform.scale(self.return_image, (100, 100))
+        self.return_image = pygame.transform.scale(self.return_image, (120, 120))
         self.return_rect = self.return_image.get_rect()
         self.return_rect.x = 50
         self.return_rect.y = 50
 
         # bouton credits
         self.credit_image = pygame.image.load(DEFAULT.path_credit)
-        self.credit_image = pygame.transform.scale(self.credit_image, (100, 100))
+        self.credit_image = pygame.transform.scale(self.credit_image, (120, 120))
         self.credit_rect = self.credit_image.get_rect()
         self.credit_rect.x = 0
         self.credit_rect.y = 0
 
         # bouton info
         self.info_image = pygame.image.load(DEFAULT.path_info)
-        self.info_image = pygame.transform.scale(self.info_image, (100, 100))
+        self.info_image = pygame.transform.scale(self.info_image, (120, 120))
         self.info_rect = self.info_image.get_rect()
-        self.info_rect.x = 50
+        self.info_rect.x = 5
         self.info_rect.y = 150
 
         # bouton play
         self.play_image = pygame.image.load(DEFAULT.path_play)
-        self.play_image = pygame.transform.scale(self.play_image, (100, 100))
+        self.play_image = pygame.transform.scale(self.play_image, (150, 150))
         self.play_rect = self.play_image.get_rect()
         self.play_rect.x = DEFAULT.window_width / 2
-        self.play_rect.y = 110
+        self.play_rect.y = 250
 
         # bouton volume on/off
         self.sound_on_image = pygame.image.load(DEFAULT.path_sound_on)
         self.sound_off_image = pygame.image.load(DEFAULT.path_sound_off)
-        self.sound_on_image = pygame.transform.scale(self.sound_on_image, (100, 100))
-        self.sound_off_image = pygame.transform.scale(self.sound_off_image, (100, 100))
+        self.sound_on_image = pygame.transform.scale(self.sound_on_image, (120, 120))
+        self.sound_off_image = pygame.transform.scale(self.sound_off_image, (120, 120))
         self.sound_rect = self.sound_on_image.get_rect()
         self.sound_rect.x = 50
         self.sound_rect.y = 170
